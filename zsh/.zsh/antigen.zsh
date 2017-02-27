81719f1ebcf36d663fc65f76917eae618b71fbd3
Jean-Marc Boite <jeanmarcboite@gmail.com>
2017-02-27 13:10:29 +0100

    remove liquidprompt

--- zsh/.zsh/antigen.zsh
+++ zsh/.zsh/antigen.zsh
@@ -7,11 +7,19 @@ antigen bundle command-not-found
 antigen bundle colorize
 antigen bundle screen
 antigen bundle djui/alias-tips
-antigen bundle b4b4r07/enhancd
+#antigen bundle b4b4r07/enhancd
+antigen bundle autojump
+antigen bundle k
+antigen bundle voronkovich/gitignore.plugin.zsh
 antigen bundle zsh-users/zsh-syntax-highlighting
 antigen bundle tarruda/zsh-autosuggestions
+antigen bundle robertzk/send.zsh
+antigen-bundle Tarrasch/zsh-bd
+antigen-bundle oknowton/zsh-dwim
+antigen-bundle seletskiy/zsh-favorite-directories
 
 antigen theme wedisagree
+#antigen bundle nojhan/liquidprompt
 
 antigen apply
 
@@ -19,3 +27,7 @@ antigen apply
 
 bindkey '^ ' autosuggest-accept
 bindkey '^x' autosuggest-execute
+
+favorite-directories:get() {
+    echo dotfiles 1 ~/dotfiles
+}
