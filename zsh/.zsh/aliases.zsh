fc8137b35310639759b90c4720c7cc5033f0ccbf
Jean-Marc Boite <jeanmarcboite@gmail.com>
2017-03-01 07:38:12 +0100

    sudo

--- zsh/.zsh/aliases.zsh
+++ zsh/.zsh/aliases.zsh
@@ -1,5 +1,3 @@
 # aliases
 
 alias df="df -h"
-alias backup="sudo /home/boite/Dropbox/etc/cron.weekly/cryptup"
-alias netman="sudo service network-manager restart"
---
+++ zsh/.zsh/sudo.zsh
@@ -0,0 +1,7 @@
+# sudo.zsh
+
+alias backup="sudo /home/boite/Dropbox/etc/cron.weekly/cryptup"
+
+alias get="sudo apt-get install -y"
+
+alias netman="sudo service network-manager restart"
