b26b7a8bcf3a5316f6e8755dd824cd3103fc7f84
jeanmarcboite <jeanmarc.boite@gmail.com>
2025-10-25 18:50:19 +0200

    Add .config/zsh/custom/opts.zsh

---
+++ private_dot_config/zsh/custom/opts.zsh
@@ -0,0 +1,11 @@
+setopt nobeep
+setopt auto_cd
+setopt append_history
+export HISTSIZE=20000000
+export HISTFILE=~/.zsh_history
+export SAVEHIST=20000000
+setopt inc_append_history
+setopt hist_ignore_all_dups
+setopt hist_ignore_space
+setopt share_history
+setopt extendedglob dotglob
